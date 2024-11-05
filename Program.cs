f6758f85ed1416e766782f52a80ac1e696efa034
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-05 10:37:59 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment_1._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a simple console application and declare basic datatypes like int, float, decimal, double and print their min and max values.
+            Console.WriteLine("Int - Min: " + int.MinValue + ", Max: " + int.MaxValue);
+            Console.WriteLine("Float - Min: " + float.MinValue + ", Max: " + float.MaxValue);
+            Console.WriteLine("Decimal - Min: " + decimal.MinValue + ", Max: " + decimal.MaxValue);
+            Console.WriteLine("Double - Min: " + double.MinValue + ", Max: " + double.MaxValue);
+        }
+    }
+}
